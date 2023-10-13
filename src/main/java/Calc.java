30c5b6fed877b0d8cdd176ff7ae8eb227f2ff6df
Andrew-Moskalew <Lorenzo4474@gmail.com>
2023-10-14 01:07:47 +0300

    Initial commit

---
+++ src/main/java/Calc.java
@@ -0,0 +1,6 @@
+public class Calc {
+    public String sum(String a, String b) {
+        String result = Integer.toString(Integer.parseInt(a)+Integer.parseInt(b));
+        return result;
+    }
+}
